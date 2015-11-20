2270a2162ab9525831e587fcb7143d4203dd8002
za <za@python.or.id>
2015-11-21 10:48:12 +1100

    Postgresql Setup
    * install psycopg2
    * pip install psycopg2
    
    Create role, database
    
        $ sudo -u postgres psql
        postgres# CREATE ROLE dd3 WITH LOGIN CREATEDB PASSSWORD 'dd3';
        CREATE ROLE
        postgres# CREATE DATABASE dd3;
        CREATE DATABASE
    
    Login as dd3
    
        $ psql -U dd3 -h localhost -W
        password for user dd3:
        psql (9.3.10)
        dd3=>

--- dd3/dd3/settings.py
+++ dd3/dd3/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'dd3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dd3',
+		'USER': 'dd3',
+		'PASSWORD': 'dd3',
+		'HOST': '127.0.0.1',
+		'PORT': '5432',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
